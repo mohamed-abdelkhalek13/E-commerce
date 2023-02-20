8a87cdd6dd8d40781d94c7723b1344ea73beaf84
mohamed-abdelkhalek <mohamedabdelkhalek1313@gmail.com>
2023-02-20 09:51:06 +0200

    project initialization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './Components/Pages/cart/cart.component';
+import { HomeComponent } from './Components/Pages/home/home.component';
+import { LoginComponent } from './Components/Pages/login/login.component';
+import { OrdersComponent } from './Components/Pages/orders/orders.component';
+import { ProductDetailsComponent } from './Components/Pages/product-details/product-details.component';
+import { ProductsComponent } from './Components/Pages/products/products.component';
+import { RegisterComponent } from './Components/Pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"login", component:LoginComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"products", component:ProductsComponent},
+  {path:"products/:id", component:ProductDetailsComponent},
+  {path:"cart", component:CartComponent},
+  {path:"orders", component:OrdersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/Shared/header/header.component';
+import { FooterComponent } from './Components/Shared/footer/footer.component';
+import { FilterComponent } from './Components/Shared/filter/filter.component';
+import { HomeComponent } from './Components/Pages/home/home.component';
+import { ProductsComponent } from './Components/Pages/products/products.component';
+import { ProductDetailsComponent } from './Components/Pages/product-details/product-details.component';
+import { CartComponent } from './Components/Pages/cart/cart.component';
+import { OrdersComponent } from './Components/Pages/orders/orders.component';
+import { LoginComponent } from './Components/Pages/login/login.component';
+import { RegisterComponent } from './Components/Pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FilterComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    OrdersComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
