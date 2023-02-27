6e4538e26a1430f39c3e98a3c42702723fbcda42
mohamed-abdelkhalek <mohamedabdelkhalek1313@gmail.com>
2023-02-27 12:30:59 +0200

    last update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,35 @@ import { CartComponent } from './Components/Pages/cart/cart.component';
 import { OrdersComponent } from './Components/Pages/orders/orders.component';
 import { LoginComponent } from './Components/Pages/login/login.component';
 import { RegisterComponent } from './Components/Pages/register/register.component';
+import { SliderComponent } from './Components/Pages/slider/slider.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CategoriesComponent } from './Components/Pages/categories/categories.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './Components/Pages/shipping/shipping.component';
+import { CheckoutComponent } from './Components/Pages/checkout/checkout.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAccordionModule, NgbDatepickerModule, NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { TransactionsComponent } from './Components/Pages/transactions/transactions.component';
+import {MatTableModule} from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ProductCardComponent } from './Components/Shared/product-card/product-card.component';
+import { ProductReviewsComponent } from './Components/Shared/product-reviews/product-reviews.component';
+import { ProductDetailsCardComponent } from './Components/Shared/product-details-card/product-details-card.component';
 
 @NgModule({
   declarations: [
+    ProductCardComponent,
+    ProductDetailsCardComponent,
+    ProductReviewsComponent,
     AppComponent,
     HeaderComponent,
     FooterComponent,
@@ -25,11 +51,32 @@ import { RegisterComponent } from './Components/Pages/register/register.componen
     CartComponent,
     OrdersComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    SliderComponent,
+    CategoriesComponent,
+    ShippingComponent,
+    CheckoutComponent,
+    TransactionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    NgbAccordionModule,
+    HttpClientModule,
+    NgbDatepickerModule,
+    NgbModule,
+    NgbCarouselModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule,
+    MatPaginatorModule,
+
+
   ],
   providers: [NgbModal],
   bootstrap: [AppComponent]
