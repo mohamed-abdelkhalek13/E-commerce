8d5bfe3c1f5f51118259807bcc3ab2406bda65d2
AhmedEl-gendy <ahmedayman0794@gmail.com>
2023-03-03 01:14:27 +0200

    edit pages

--- src/app/Components/Pages/customers/customers.component.ts
+++ src/app/Components/Pages/customers/customers.component.ts
@@ -1,39 +1,53 @@
 import { Component } from '@angular/core';
 import { map } from 'rxjs';
-import { CustomerService } from '../../../Services/customer.service';
+import { CustomerService } from 'src/app/services/customer.service';
+
 export interface PeriodicElement {
-  id:number;
+  id: number;
   Name: string;
   Email: string;
   ShippingAddress: string;
-  Country:string;
-  Gender:string;
+  Country: string;
+  Gender: string;
 }
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.css']
+  styleUrls: ['./customers.component.css'],
 })
 export class CustomersComponent {
-  ELEMENT_DATA:any;
-  constructor(private customerService:CustomerService){
-    this.customerService.getAllCustomers().pipe(map((data)=>{
-      const CustomerList=[];
-      for(const key in data){
-        if(data.hasOwnProperty(key)){
-          CustomerList.push(data[key]);
-        }
-      } 
-      console.log(CustomerList);
-      return CustomerList;   
-    })).subscribe((CustomerList)=>{
-      this.ELEMENT_DATA=CustomerList
-    });  
+  ELEMENT_DATA: any;
+  constructor(private customerService: CustomerService) {
+    this.customerService
+      .getAllCustomers()
+      .pipe(
+        map((data) => {
+          const CustomerList = [];
+          for (const key in data) {
+            if (data.hasOwnProperty(key)) {
+              CustomerList.push(data[key]);
+            }
+          }
+          console.log(CustomerList);
+          return CustomerList;
+        })
+      )
+      .subscribe((CustomerList) => {
+        this.ELEMENT_DATA = CustomerList;
+      });
     console.log(this.ELEMENT_DATA);
-    return this.ELEMENT_DATA;;
+    return this.ELEMENT_DATA;
   }
-  deleteCustomer(email:any){
-      this.customerService.deleteCustomer(email);
+  deleteCustomer(email: any) {
+    this.customerService.deleteCustomer(email);
   }
-  displayedColumns: string[] = ['Position', 'Name', 'Email', 'ShippingAddress','Country','Gender','Delete'];
+  displayedColumns: string[] = [
+    'Position',
+    'Name',
+    'Email',
+    'ShippingAddress',
+    'Country',
+    'Gender',
+    'Delete',
+  ];
 }
